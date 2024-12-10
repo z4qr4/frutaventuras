82699be23fa30ce6571e83b12d4581c00bc46fbf
Manolo Fernández <manolo.fernandez@zqr-mini.local>
2024-12-10 11:11:58 -0500

    a test

--- project/Scripts/PlayerMovement.gd
+++ project/Scripts/PlayerMovement.gd
@@ -11,12 +11,14 @@ var lat_range: float
 
 func _physics_process(delta):
 	
-	position = gps_to_godot(PlayerStats.lat, PlayerStats.lon)
+#	position = gps_to_godot(PlayerStats.lat, PlayerStats.lon)
 	
 #	var fw = int(Input.is_action_pressed("bw")) - int(Input.is_action_pressed("fw"))
 #	var sw = int(Input.is_action_pressed("sr")) - int(Input.is_action_pressed("sl"))
 #
 #	translate(Vector3(sw, 0, fw)/10)
+	
+	pass
 
 
 func _ready():
